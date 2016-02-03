300eb53bc1d3a197b87d598340b51e5a91087d94
xungeng <xungeng@gmail.com>
2016-02-03 21:14:33 +0800

    去掉没必要的class声明

--- WeChatRedEnvelop.h
+++ WeChatRedEnvelop.h
@@ -58,47 +58,11 @@
 - (id)GetCurrentLogicController;
 @end
 
-@interface CContactDB : NSObject
-{
-    NSObject *m_oMMDB;
-}
-
-- (_Bool)isFriendExtTableExist:(id)arg1;
-- (void)getFriendExtTableValue:(id)arg1 fromDBObject:(id)arg2;
-- (void)getFriendTableValue:(id)arg1 fromDBObject:(id)arg2;
-- (_Bool)SetFriendMetaFlag:(unsigned int)arg1 ForUser:(id)arg2;
-- (unsigned int)GetFriendMetaFlag:(id)arg1;
-- (_Bool)SetLastUpdateTime:(unsigned int)arg1 ForUser:(id)arg2;
-- (unsigned int)GetLastUpdateTimeOfUser:(id)arg1;
-- (_Bool)ModifyNotifyStatus:(id)arg1 withStatus:(unsigned int)arg2;
-- (_Bool)updateContact:(id)arg1 andImage:(_Bool)arg2;
-- (_Bool)updateContact:(id)arg1;
-- (_Bool)deleteContact:(id)arg1;
-- (_Bool)addContact:(id)arg1;
-- (_Bool)getAllContactNoExtInfoList:(id)arg1 listType:(unsigned int)arg2;
-- (_Bool)getAllContactList:(id)arg1 listType:(unsigned int)arg2;
-- (void)getFriendAndExtTableValue:(id)arg1 fromDBContact:(id)arg2 DBContactExt:(id)arg3;
-- (_Bool)loadContactExtInfo:(id)arg1;
-- (id)getContact:(id)arg1;
-- (_Bool)updateContactKeyExtInfo:(id)arg1;
-- (_Bool)modifyFriendExtTable:(id)arg1;
-- (_Bool)modifyFriendTable:(id)arg1;
-- (_Bool)addFriendExtTable:(id)arg1;
-- (_Bool)addFriendTable:(id)arg1;
-- (void)saveUserImage:(id)arg1;
-- (_Bool)RollbackTransaction;
-- (_Bool)CommitTransaction;
-- (_Bool)BeginTransaction;
-- (id)DBContext;
-- (void)initDB:(id)arg1;
-- (id)init;
-
-@end
-
 @interface CContact: NSObject
 @property(retain, nonatomic) NSString *m_nsUsrName;
 @property(retain, nonatomic) NSString *m_nsHeadImgUrl;
 @property(retain, nonatomic) NSString *m_nsNickName;
+
 
 - (id)getContactDisplayName;
 @end
@@ -110,20 +74,6 @@
 @end
 
 @interface CContactMgr : NSObject
-{
-    unsigned int m_uiLoadedType;
-    CContactDB *m_oContactDB;
-    NSObject *m_oContactOPLog;
-    NSRecursiveLock *m_oMainControllerLock;
-    NSMutableDictionary *m_dicContacts;
-    NSMutableDictionary *m_dicEncodeContacts;
-    _Bool m_bDataNeedReload;
-    NSMutableDictionary *m_mapLastGetTime;
-    NSMutableDictionary *m_dicHardCodeUsers;
-    unsigned int m_uiDBVersion;
-    CContact *m_oSelfContact;
-    _Bool m_bIsSelfContactUpdated;
-}
 - (id)getSelfContact;
 - (id)getContact:(id)arg1 listType:(unsigned int)arg2 contactType:(unsigned int)arg3;
 @end
